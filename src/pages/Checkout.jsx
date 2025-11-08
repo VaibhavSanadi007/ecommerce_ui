3f36c305af7894b394c48c614509fdf66ffa5454
vaibhav <vaibhavbhausanadi0007@gmail.com>
2025-11-09 00:16:49 +0530

    d

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -42,7 +42,7 @@ const Checkout = () => {
         pincode,
     }
     const data = await axios.post(
-      `/api/orders/`,{shippingAddress},
+      `/api/orders/create`,{shippingAddress},
       {
         withCredentials: true,
       }
