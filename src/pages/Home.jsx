2ca17e797b7f1af3eef1f62eca3014e0022c504e
vaibhav <vaibhavbhausanadi0007@gmail.com>
2025-11-10 19:19:03 +0530

    gg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import { useNavigate } from "react-router-dom"
 import Caraousel from "../components/Caraousel.jsx"
 import ProductCard from "../components/ProductCard.jsx"
 import { useSelector } from "react-redux";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { PiKeyReturnLight } from "react-icons/pi";
+import { MdPayment } from "react-icons/md";
+import { BiSupport } from "react-icons/bi";
+
 const Home = () => {
   const navigate = useNavigate();
 
@@ -37,6 +42,30 @@ const Home = () => {
 
       <div className="xl:h-35 h-30 flex items-center justify-center ">
         <button className="xl:h-10 xl:px-10 border transition-all duration-300 hover:bg-blue-300 hover:text-white cursor-pointer " onClick={handleHomeNav}>Shop All</button>
+      </div>
+
+      <div className="h-30 flex justify-evenly gap-5 my-5 ">
+        <div className="h-full flex flex-col justify-evenly items-center">
+          <div className="flex items-center justify-center "><LiaShippingFastSolid size={30} /></div>
+          <h1 className="text-center font-semibold  w-[80%] text-[0.9rem] ">Free Shipping</h1>
+          <h1 className="text-[0.8rem] text-gray-500">For all Orders Over  &#8377;100.</h1>
+        </div>
+        <div className="h-full flex flex-col justify-evenly items-center">
+          <div className="flex items-center justify-center"><PiKeyReturnLight size={30} /></div>
+          <h1 className="text-center font-semibold  w-[80%] text-[0.9rem] ">30 Days Returns</h1>
+          <h1 className="text-[0.8rem] text-gray-500">For an Exchange Product.</h1>
+        </div>
+        <div className="h-full flex flex-col justify-evenly items-center">
+          <div className="flex items-center justify-center"><MdPayment size={30} /></div>
+          <h1 className="text-center font-semibold  w-[80%] text-[0.9rem] ">Secured Payment</h1>
+          <h1 className="text-[0.8rem] text-gray-500">Payment Cards Accepted.</h1>
+        </div>
+        <div className="h-full flex flex-col justify-evenly items-center">
+          <div className="flex items-center justify-center"><BiSupport size={30} /></div>
+          <h1 className="text-center font-semibold  w-[80%] text-[0.9rem] ">Support 24/7</h1>
+          <h1 className="text-[0.8rem] text-gray-500">Contact us Anytime.</h1>
+        </div>
+      
       </div>
 
     </div>
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 
 export const auth_url = import.meta.env.VITE_AUTH_URL;
-export const prod_url = import.meta.env.VITE_PRODUCT_URL;
+export const prod_url = `http://localhost:3001`;
 export const cart_url = import.meta.env.VITE_CART_URL;
 export const order_url = import.meta.env.VITE_ORDER_URL;
 export const payment_url = import.meta.env.VITE_PAYMENT_URL;
